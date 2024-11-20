8f8e29b0c20ffc192c035f3d02c72bc05b1e276e
elkhailihamza <elkhailihamza331@gmail.com>
2024-11-20 14:57:15 +0100

    CIT-3 added ferme crud

--- src/main/java/org/project/citronix/entity/Ferme.java
+++ src/main/java/org/project/citronix/entity/Ferme.java
@@ -1,12 +1,18 @@
 package org.project.citronix.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 @Table(name = "Ferme")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Ferme {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,6 +21,13 @@ public class Ferme {
     private String superficie;
     private String localisation;
     private LocalDateTime date_de_creation;
+
+    @PrePersist
+    public void prePersist() {
+        if (date_de_creation == null) {
+            date_de_creation = LocalDateTime.now();
+        }
+    }
 
     @OneToMany(mappedBy = "ferme")
     private List<Champ> champs;
