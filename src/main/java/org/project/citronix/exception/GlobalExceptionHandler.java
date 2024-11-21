26fb008237ab77f2d14fbe12323e1223e1af5a1e
elkhailihamza <elkhailihamza331@gmail.com>
2024-11-21 14:24:33 +0100

    added superficie non compatible exception

--- src/main/java/org/project/citronix/exception/GlobalExceptionHandler.java
+++ src/main/java/org/project/citronix/exception/GlobalExceptionHandler.java
@@ -16,4 +16,10 @@ public class GlobalExceptionHandler {
         ExceptionDetails exceptionDetails = new ExceptionDetails(ex.getMessage(), new Date(), request.getDescription(false));
         return new ResponseEntity<>(exceptionDetails, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(SuperficieNonCompatible.class)
+    public ResponseEntity<ExceptionDetails> handleEntityNotFoundException(SuperficieNonCompatible ex, WebRequest request) {
+        ExceptionDetails exceptionDetails = new ExceptionDetails(ex.getMessage(), new Date(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionDetails, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
 }
---
+++ src/main/java/org/project/citronix/exception/SuperficieNonCompatible.java
@@ -0,0 +1,7 @@
+package org.project.citronix.exception;
+
+public class SuperficieNonCompatible extends RuntimeException {
+    public SuperficieNonCompatible(String message) {
+        super(message);
+    }
+}
