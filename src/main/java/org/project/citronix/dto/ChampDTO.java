46111a5c6f0245cb096511514bd31463cb8e6415
elkhailihamza <elkhailihamza331@gmail.com>
2024-11-21 14:25:55 +0100

    changed superficie validation in both dtos

--- src/main/java/org/project/citronix/dto/ChampDTO.java
+++ src/main/java/org/project/citronix/dto/ChampDTO.java
@@ -1,5 +1,7 @@
 package org.project.citronix.dto;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.groups.Default;
@@ -22,8 +24,9 @@ public class ChampDTO {
     @NotNull(message = "Id needs to not be null", groups = {Delete.class, Update.class, Associate.class})
     private long id;
 
-    @NotNull(message = "Superficie needs to not be null", groups = {Create.class, Update.class})
-    private String superficie;
+    @DecimalMin(value = "0.1", message = "Superficie must be greater than or equal to 0.1", groups = {Create.class, Update.class})
+    @DecimalMax(value = "100000.0", message = "Superficie must be less than or equal to 100,000", groups = {Create.class, Update.class})
+    private double superficie;
 
     @NotNull(message = "Ferme id needs to not be null", groups = {Update.class, Associate.class})
     private long fermeId;
